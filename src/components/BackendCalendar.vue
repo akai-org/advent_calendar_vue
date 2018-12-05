9205a361b45025d9baee86f3dd1e571e09fb6911
Dawid Dziedzic <dziedzicdev@gmail.com>
2018-12-05 22:49:26 +0100

    Added little backend calendar but idk what am I doing so I'm just going to get some sleep

--- src/components/BackendCalendar.vue
+++ src/components/BackendCalendar.vue
@@ -1,6 +1,106 @@
 <template>
-    <div class="backend-calendar">
-
+    <div class="backend-calendar" id="backend-calendar">
+        <div class="container-fluid">
+            <div class="calendar-heading">
+                Backend
+                <div class="subheading">
+                    Dla wszystkich tych, dla których PHP jest fajne!
+                </div>
+            </div>
+            <div class="card calendar-place">
+                <div class="calendar-place_heading">
+                    Backendowy kalendarz adwentowy
+                </div>
+                <div class="calendar">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 calendar-tile-container">
+                                <div class="calendar-tile"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
